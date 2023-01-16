a1fc6f690e64f979afbe5fbd71ad5e8a6cee9c1e
wed-4 <94733396+wed-4@users.noreply.github.com>
2023-01-17 02:16:19 +0900

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt install build-essential libbz2-dev libdb-dev libreadline-dev libffi-dev libgdbm-dev liblzma-dev libncursesw5-dev libsqlite3-dev libssl-dev zlib1g-dev uuid-dev tk-dev
+sudo apt install python3-pip
+sudo pip install requests
+sudo pip install dearpygui
